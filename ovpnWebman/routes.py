d1ec9bb8a54a631cc77a79036a60aa30c4b2b30f
Alan Platt <aplatt@equalexperts.com>
2017-10-18 12:15:55 +0100

    add app routes
    / GET to root url shows main import form
    / POST currently checks the password and displays if valid as a test
    
    /get-file GET will download ovpn file. This is just a place holder and
    doesnt do anything as present

---
+++ ovpnWebman/routes.py
@@ -0,0 +1,34 @@
+from ovpnWebman import app
+from flask import render_template, url_for, request
+from ovpnWebman.helpers import check_password
+
+@app.route('/', methods=['GET','POST'])
+def submit():
+  if request.method == 'POST':
+    print (dir(request.form))
+    print (request.form)
+    foo = run_ls()
+    print (foo.decode('utf-8'))
+
+    if request.form['password1'] == request.form['password2']:
+      if check_password(request.form['password1']):
+        return request.form['password1']
+      else:
+        error='Password of insufficient complexity'
+    else:
+      error='password mismatch'
+    return render_template('submit.html.j2', error=error)
+
+  elif request.method == 'GET':
+    return render_template('submit.html.j2')
+
+
+#TODO download the ovpn cert
+@app.route("/get-file")
+def get_file():
+    filedata = generate_file_data()
+
+    return Response(filedata,
+                       mimetype="text/plain",
+                       headers={"Content-Disposition":
+                                    "attachment;filename=test.txt"})

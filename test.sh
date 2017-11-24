b262551e1c7aef5bc46fffe24842cd26bb3ed41a
Alan Platt <aplatt@equalexperts.com>
2017-11-24 17:10:39 +0000

    fix test script to work with travis

--- test.sh
+++ test.sh
@@ -3,29 +3,22 @@
 # Exit on any error
 set -e
 
-
-if [[ $TRAVIS != 'true' ]]; then
-
-  VENV_NAME=".venv"
-  if ! [ -e "${VENV_NAME}/bin/activate" ]; then
-    echo "Creating virtualenv..."
-    virtualenv -p python3 ${VENV_NAME}
-  fi
-
-  source "${VENV_NAME}/bin/activate"
-
-  echo "Installing requirements..."
-  pip install -q -r requirements.txt -r requirements-tests.txt
-
+VENV_NAME=".venv"
+if ! [ -e "${VENV_NAME}/bin/activate" ]; then
+  echo "Creating virtualenv..."
+  virtualenv -p python3 ${VENV_NAME}
 fi
+
+source "${VENV_NAME}/bin/activate"
+
+echo "Installing requirements..."
+pip install -q -r requirements.txt -r requirements-tests.txt
 
 echo "Cleaning .pyc files..."
 find . -iname "*.pyc" -delete
 
 echo "Running tests..."
 python -m pytest tests/
-#pytest tests
-#nosetests --with-coverage --cover-erase --cover-package=aws_assume_role
 
 echo "Running flake8..."
 flake8 *.py ovpnWebman/*.py --exclude __init__.py --ignore E501
